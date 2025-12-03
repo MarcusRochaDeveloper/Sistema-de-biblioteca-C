5c5af86bcb8ab6441a57761c13a4202e14baaaa2
Marcus Rocha <mvcsr2208@hotmail.com>
2025-12-03 12:13:44 -0300

    Add initial definitions and constants for the library system

---
+++ biblioteca.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+//========== DEFINICOES E CONSTANTES==========
+
+#define MAX_LIVROS 1000
+#define MAX_USUARIOS 1000
+#define MAX_EMPRESTIMOS 2000
+
+//========== TAMANHOS MAXIMOS PARA STRINGS COM ESPACO PARA CARACTER NULO==========
+
+#define MAX_TITULO 101
+#define MAX_AUTOR 81
+#define MAX_EDITORA 61
+#define MAX_NOME 101
+#define MAX_CURSO 51
+#define MAX_TEL 16
